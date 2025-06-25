85548559ef05b384653ac6fe9eb6dbc7d1491573
Febry Dwi Putra <febryituery@gmail.com>
2025-06-25 09:38:21 +0700

    [Febry]

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,45 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
+import React, { useState } from 'react';
+import ProductList from './ProductList';
+import './App.css';
+import AboutUs from './AboutUs';
 
-import { AppProvider } from './context/AppContext';
-import CartValue from './components/CartValue';
-import ExpenseList from './components/ExpenseList';
-import ItemSelected from './components/ItemSelected';
-import Location from './components/Location';
+function App() {
+  
+  const [showProductList, setShowProductList] = useState(false);
 
-const App = () => {
-    return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Shopping App</h1>
-                <div className='row mt-3'> 
-                    <div className='col-sm'>
-                        <CartValue />
-                    </div>
-                    <div className='col-sm'>
-                        <Location />
-                    </div>
-                </div>
-                <h3 className='mt-3'>Shopping Cart</h3>
-                <div className='row '>
-                    <div className='col-sm'>
-                        <ExpenseList />
-                    </div>
-                </div>
-                <h3 className='mt-3'>Add Items</h3>
-                <div className='row mt-3'>
-                    <div className='col-sm'>
-                        <ItemSelected/>
-                    </div>
-                </div>
-            </div>
-        </AppProvider>
-    );
-};
+  const handleGetStartedClick = () => {
+    setShowProductList(true);
+  };
+
+  return (
+    <div className="app-container">
+      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+        <div className="background-image"></div>
+        <div className="content">
+         <div className="landing_content">
+         <h1>Welcome To Paradise Nursery</h1>
+          <div className="divider"></div>
+          <p>Where Green Meets Serenity</p>
+         
+          <button className="get-started-button" onClick={handleGetStartedClick}>
+            Get Started
+          </button>
+         </div>
+          <div className="aboutus_container">
+          <AboutUs/>
+          </div>
+          </div>
+
+      </div>
+      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+        <ProductList />
+      </div>
+    </div>
+  );
+}
+
 export default App;
+
+
+
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: "/paradise-nursery-shopping-cart-app",
+    plugins: [react()],
 })
